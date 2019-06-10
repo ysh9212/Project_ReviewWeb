325faa875f30265e01feb299fdc0f4bd89582728
riririririm <48780849+riririririm@users.noreply.github.com>
2019-06-10 18:23:13 +0900

    6:23

--- src/com/project/community/CommunityDAO.java
+++ src/com/project/community/CommunityDAO.java
@@ -8,33 +8,20 @@ import java.util.List;
 
 import com.project.board.BoardDAO;
 import com.project.board.BoardDTO;
+import com.project.community.board.cBoardDTO;
+import com.project.community.bug.BugDTO;
 import com.project.community.notice.NoticeDTO;
 import com.project.community.qna.QnaDTO;
+import com.project.community.review.ReviewDTO;
 import com.project.community.used.UsedDTO;
 import com.project.shopPage.SearchRow;
 import com.project.util.DBConnector;
 
 public class CommunityDAO implements BoardDAO{
 
-	@Override
-	public int getNum() throws Exception {
-		int result=0;
-		Connection con = DBConnector.getConnect();
-		String sql = "select * from dual";
-		PreparedStatement st = con.prepareStatement(sql);
-		ResultSet rs = st.executeQuery();
-		rs.next();
-		result = rs.getInt(1);
-		DBConnector.disConnect(rs, st, con);
-		return 0;
-	}
-
-	@Override
-	public int getTotalCount(SearchRow searchRow, Connection con) throws Exception {
-		int result=0;
-		return result;
-	}
-	// 자유게시판 가져오는 용도;
+	// 리스트 불러오기
+	
+	// 공지사항;
 	@Override
 	public List<BoardDTO> selectList(SearchRow searchRow, Connection con) throws Exception {
 		List<BoardDTO> ar = new ArrayList<BoardDTO>();
@@ -43,62 +30,143 @@ public class CommunityDAO implements BoardDAO{
 		ResultSet rs = st.executeQuery();
 		return ar;
 	}
-	
-	// 자유게시판 가져오는 용도;
-	public List<NoticeDTO> noticeList(SearchRow searchRow, Connection con) throws Exception{
-		List<NoticeDTO> ar = new ArrayList<NoticeDTO>();
-		String sql = "select title from community_notice order by desc";
+	// 자유게시판;
+	public List<cBoardDTO> noticeList(SearchRow searchRow, Connection con) throws Exception{
+		List<cBoardDTO> ar = new ArrayList<cBoardDTO>();
+		String sql = "select title from community_board order by desc";
 		PreparedStatement st = con.prepareStatement(sql);
 		ResultSet rs = st.executeQuery();
 		return ar;
 	}
-	
-	// QnA 가저오는 용도;
-	public List<QnaDTO> qnaList(SearchRow searchRow, Connection con) throws Exception{
-		List<QnaDTO> ar = new ArrayList<QnaDTO>();
-		String sql = "select title from community_qna order by desc";
+	// 유저리뷰;
+	public List<ReviewDTO> reviewList(SearchRow searchRow, Connection con) throws Exception{
+		List<ReviewDTO> ar = new ArrayList<ReviewDTO>();
+		String sql = "select title from community_review order by desc";
 		PreparedStatement st = con.prepareStatement(sql);
 		ResultSet rs = st.executeQuery();
 		return ar;
 	}
-	
-	// used 가져오는 용도;
+	// 중고장터; - 사진을 가져오기 때문에 sql문 수정이 필요함;
 	public List<UsedDTO> usedList(SearchRow searchRow, Connection con) throws Exception{
 		List<UsedDTO> ar = new ArrayList<UsedDTO>();
 		String sql = "select title from community_used order by desc";
 		PreparedStatement st = con.prepareStatement(sql);
 		ResultSet rs = st.executeQuery();
 		return ar;
-		
 	}
+	
+	// 게시글 이동;
+	
+	// 자유게시판;
 	@Override
 	public BoardDTO selectOne(int no, Connection con) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		BoardDTO boardDTO = new BoardDTO();
+		String sql = "select * from community_notice where no=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, no);
+		ResultSet rs = st.executeQuery();
+		if(rs.next()) {
+			boardDTO.setNo(rs.getInt("no"));
+			boardDTO.setTitle(rs.getString("title"));
+			boardDTO.setWriter(rs.getString("writer"));
+			boardDTO.setReg_date(rs.getString("reg_date"));
+			boardDTO.setHit(rs.getInt("hit"));
+			boardDTO.setRecommend(rs.getInt("recommend"));
+			boardDTO.setDecommend(rs.getInt("decommend"));
+			boardDTO.setContents(rs.getString("contents"));
+		}
+		rs.close();
+		st.close();
+		return boardDTO;
+	}
+	// 공지사항;
+	public NoticeDTO boardSelectOne(int no, Connection con) throws Exception {
+		NoticeDTO noticeDTO = new NoticeDTO();
+		String sql = "select * from community_board where no=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, no);
+		ResultSet rs = st.executeQuery();
+		if(rs.next()) {
+			noticeDTO.setNo(rs.getInt("no"));
+			noticeDTO.setTitle(rs.getString("title"));
+			noticeDTO.setWriter(rs.getString("writer"));
+			noticeDTO.setReg_date(rs.getString("reg_date"));
+			noticeDTO.setHit(rs.getInt("hit"));
+			noticeDTO.setRecommend(rs.getInt("recommend"));
+			noticeDTO.setDecommend(rs.getInt("decommend"));
+			noticeDTO.setContents(rs.getString("contents"));
+		}
+		rs.close();
+		st.close();
+		return noticeDTO;
+	}
+	// 유저리뷰;
+	public ReviewDTO reviewSelectOne(int no, Connection con) throws Exception {
+		ReviewDTO reviewDTO = new ReviewDTO();
+		String sql = "select * from community_review where no=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, no);
+		ResultSet rs = st.executeQuery();
+		if(rs.next()) {
+			// reviewDTO.setContents(rs.getString("contents"));
+		}
+		rs.close();
+		st.close();
+		return reviewDTO;
+	}
+	// 버그리포트;
+	public BugDTO bugSelectOne(int no, Connection con) throws Exception {
+		BugDTO bugDTO = new BugDTO();
+		String sql = "select * from community_bug where no=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, no);
+		ResultSet rs = st.executeQuery();
+		if(rs.next()) {
+			
+		}
+		return bugDTO;
+	}
+	// 중고장터;
+	public UsedDTO usedSelectOne(int no, Connection con) throws Exception {
+		UsedDTO usedDTO = new UsedDTO();
+		String sql = "select * from community_used where no=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, no);
+		ResultSet rs = st.executeQuery();
+		if(rs.next()) {
+			
+		}
+		return usedDTO;
 	}
 
+	
+	// 안씀
+	@Override
+	public int getNum() throws Exception {
+		return 0;
+	}
+	@Override
+	public int getTotalCount(SearchRow searchRow, Connection con) throws Exception {
+		return 0;
+	}
 	@Override
 	public int insert(BoardDTO boardDTO, Connection con) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 	@Override
 	public int update(BoardDTO boardDTO, Connection con) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 	@Override
 	public int updateHit(int no, Connection con) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 	@Override
 	public int delete(int no, Connection con) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
--- src/com/project/community/board/cBoardDAO.java
+++ src/com/project/community/board/cBoardDAO.java
@@ -1,37 +1,66 @@
 package com.project.community.board;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.project.action.ActionForward;
 import com.project.board.BoardDAO;
 import com.project.board.BoardDTO;
 import com.project.shopPage.SearchRow;
+import com.project.util.DBConnector;
 
 public class cBoardDAO implements BoardDAO{
 
 	@Override
 	public int getNum() throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		Connection con = DBConnector.getConnect();
+		String sql = "";
+		PreparedStatement st = con.prepareStatement(sql);
+		ResultSet rs = st.executeQuery();
+		rs.next();
+		result = rs.getInt(1);
+		DBConnector.disConnect(rs, st, con);
+		return result;
 	}
 
 	@Override
 	public int getTotalCount(SearchRow searchRow, Connection con) throws Exception {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public List<BoardDTO> selectList(SearchRow searchRow, Connection con) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<BoardDTO> ar = new ArrayList<BoardDTO>();
+		String sql = "select no from community_board order by desc";
+		PreparedStatement st = con.prepareStatement(sql);
+		ResultSet rs = st.executeQuery();
+		return ar;
 	}
 
 	@Override
 	public BoardDTO selectOne(int no, Connection con) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		BoardDTO boardDTO = new BoardDTO();
+		String sql = "select * from community_notice where no=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, no);
+		ResultSet rs = st.executeQuery();
+		if(rs.next()) {
+			boardDTO.setNo(rs.getInt("no"));
+			boardDTO.setTitle(rs.getString("title"));
+			boardDTO.setWriter(rs.getString("writer"));
+			boardDTO.setReg_date(rs.getString("reg_date"));
+			boardDTO.setHit(rs.getInt("hit"));
+			boardDTO.setRecommend(rs.getInt("recommend"));
+			boardDTO.setDecommend(rs.getInt("decommend"));
+			boardDTO.setContents(rs.getString("contents"));
+		}
+		rs.close();
+		st.close();
+		return boardDTO;
 	}
 
 	@Override
