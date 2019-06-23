00059eccee7afea13470f9dddc8a98a15b22eb21
zombe14 <48780782+zombe14@users.noreply.github.com>
2019-06-23 23:45:43 +0900

    관리자 페이지 수정중

--- src/com/project/control/IndexAdminController.java
+++ src/com/project/control/IndexAdminController.java
@@ -57,17 +57,18 @@ public class IndexAdminController extends HttpServlet {
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String command = request.getPathInfo();
+		System.out.println(command);
 		ActionForward actionForward = null;
 		// 공지사항;
 		if(command.equals("/admin/communityNotice")) {
 			actionForward = noticeService.list(request, response);
 		}else if(command.equals("/admin/communityNoticeSelect")) {
 			actionForward = noticeService.select(request, response);
-		}else if(command.equals("/notice/communityNoticeWrite")) {
+		}else if(command.equals("/admin/communityNoticeWrite")) {
 			actionForward = noticeService.insert(request, response);
-		}else if(command.equals("/notice/communityNoticeUpdate")) {
+		}else if(command.equals("/admin/communityNoticeUpdate")) {
 			actionForward = noticeService.update(request, response);
-		}else if(command.equals("/notice/communityNoticeDelete")) {
+		}else if(command.equals("/admin/communityNoticeDelete")) {
 			actionForward = noticeService.delete(request, response);
 		//자유게시판;
 		}else if(command.equals("/admin/communityBoard")) {
@@ -80,6 +81,15 @@ public class IndexAdminController extends HttpServlet {
 			actionForward = comBoardService.update(request, response);
 		}else if(command.equals("/admin/communityBoardDelete")){
 			actionForward = comBoardService.delete(request, response);
+		// 자유게시판 댓글;
+		}else if(command.equals("/admin/communityBoardCommentsList")) {
+			actionForward = comBoardCommentsService.list(request, response);
+		}else if(command.equals("/admin/communityBoardCommentsInsert")){
+			actionForward = comBoardCommentsService.insert(request, response);
+		}else if(command.equals("/admin/communityBoardCommentsUpdate")) {
+			actionForward = comBoardCommentsService.update(request, response);
+		}else if(command.equals("/admin/communityBoardCommentsDelete")) {
+				actionForward = comBoardCommentsService.delete(request, response);
 		// 사용자 리뷰;
 		}else if(command.equals("/admin/communityReview")) {
 			actionForward = reviewService.list(request, response);
@@ -91,6 +101,14 @@ public class IndexAdminController extends HttpServlet {
 			actionForward = reviewService.update(request, response);
 		}else if(command.equals("/admin/communityReviewDelete")){
 			actionForward = reviewService.delete(request, response);
+		}else if(command.equals("/admin/communityReviewCommentsList")){
+			actionForward = reviewCommentsService.list(request, response);
+		}else if(command.equals("/admin/communityReviewCommentsInsert")){
+			actionForward = reviewCommentsService.insert(request, response);
+		}else if(command.equals("/admin/communityReviewCommentsUpdate")){
+			actionForward = reviewCommentsService.update(request, response);
+		}else if(command.equals("/admin/communityReviewCommentsDelete")){
+			actionForward = reviewCommentsService.delete(request, response);
 		// 중고 게시판;
 		}else if(command.equals("/admin/communityUsed")) {
 			actionForward = usedService.list(request, response);
@@ -103,10 +121,10 @@ public class IndexAdminController extends HttpServlet {
 		}else if(command.equals("/admin/communityusedDelete")){
 			actionForward = usedService.delete(request, response);
 		// 중고게시판 댓글;
-		}else if(command.equals("/communityComments/reviewCommentsList")){
-		}else if(command.equals("/communityComments/reviewCommentsInsert")){
-		}else if(command.equals("/communityComments/reviewCommentsUpdate")){
-		}else if(command.equals("/communityComments/reviewCommentsDelete")){	
+		}else if(command.equals("/admin/reviewCommentsList")){
+		}else if(command.equals("/admin/reviewCommentsInsert")){
+		}else if(command.equals("/admin/reviewCommentsUpdate")){
+		}else if(command.equals("/admin/reviewCommentsDelete")){	
 		// QnA;
 		}else if(command.equals("/admin/communityQna")) {
 			actionForward = qnaService.list(request, response);
@@ -118,6 +136,14 @@ public class IndexAdminController extends HttpServlet {
 			actionForward = qnaService.update(request, response);
 		}else if(command.equals("/admin/communityQnaDelete")){
 			actionForward = qnaService.delete(request, response);
+		}else if(command.equals("/admin/communityQnaCommentsList")) {
+			actionForward = qnaCommentsService.list(request, response);
+		}else if(command.equals("/admin/communityQnaCommentsInsert")){
+			actionForward = qnaCommentsService.insert(request, response);
+		}else if(command.equals("/admin/communityQnaCommentsUpdate")) {
+			actionForward = qnaCommentsService.update(request, response);
+		}else if(command.equals("/admin/communityQnaCommentsDelete")) {
+			actionForward = qnaCommentsService.delete(request, response);
 		// 버그 리포트;
 		}else if(command.equals("/admin/communityBug")) {
 			actionForward = bugService.list(request, response);
@@ -134,30 +160,26 @@ public class IndexAdminController extends HttpServlet {
 		}else if(command.equals("/admin/communityBugDecommend")){
 			actionForward = bugService.decommend(request, response);
 		// 버그 리포트 댓글;
-		}else if(command.equals("/communityComments/communityBugCommentsList")) {
+		}else if(command.equals("/admin/communityBugCommentsList")) {
 			actionForward = bugCommentsService.list(request, response);
-		}else if(command.equals("/communityComments/communityBugCommentsInsert")){
+		}else if(command.equals("/admin/communityBugCommentsInsert")){
 			actionForward = bugCommentsService.insert(request, response);
-		}else if(command.equals("/communityComments/communityBugCommentsUpdate")) {
+		}else if(command.equals("/admin/communityBugCommentsUpdate")) {
 			actionForward = bugCommentsService.update(request, response);
-		}else if(command.equals("/communityComments/communityBugCommentsDelete")) {
+		}else if(command.equals("/admin/communityBugCommentsDelete")) {
 			actionForward = bugCommentsService.delete(request, response);
 		}else {
-			actionForward = new ActionForward(); // ?? �̰� ����? �ʱ�ȭ�ΰ�?
+			actionForward = new ActionForward();
 		}
-		// service���� �ذ��� �� �ٽ� return���� actionforward�� �̿��� requesetdispatcher �� ����;
-		// �׷��� path�� �����ؼ� ��� �� �� �ִµ�?
 		if(actionForward.isCheck()) {
 			RequestDispatcher view = request.getRequestDispatcher(actionForward.getPath());
 			view.forward(request, response);
 		}else {
 			response.sendRedirect(actionForward.getPath());
 		}
-		
 	} 	    	
     	// RequestDispatcher view = request.getRequestDispatcher("./WEB-INF/views/admin/main.jsp");
 		// view.forward(request, response);
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
